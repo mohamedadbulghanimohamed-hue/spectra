0190217e426a3b1be8172c68133c5fc8eabce525
mohamedadbulghanimohamed-hue <mohamedadbulghanimohamed@gmail.com>
2025-11-19 13:48:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,43 @@ active.forEach (function (a) {
             active[i].classList.remove("active");            
         };
         this.classList.add("active");
+    };
+});
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 7) {
+    document.querySelector(".header").classList.add("ef");
+    document.querySelector(".landing .logo").classList.add("move");
+    document.querySelector(".header img").style = "opacity: 1; transform: translateY(0px);";
+    }else {
+        document.querySelector(".header").classList.remove("ef");     
+        document.querySelector(".landing .logo").classList.remove("move");
+        document.querySelector(".header img").style = "opacity: 0; transform: translateY(100px);";
     }
-})
+});
+const cards = document.querySelectorAll(".info .card");
+const observer = new IntersectionObserver((entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        if (entries[i].isIntersecting) {
+            entries[i].target.classList.add("up");
+        }else {
+            entries[i].target.classList.remove("up");
+        }
+    };
+});
+cards.forEach(card => {
+    observer.observe(card);
+});
+
+const h2 = document.querySelectorAll("h2");
+const observer2 = new IntersectionObserver((entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        if (entries[i].isIntersecting) {
+            entries[i].target.classList.add("right");
+        }else {
+            entries[i].target.classList.remove("right");
+        }
+    };
+});
+h2.forEach(h2 => {
+    observer2.observe(h2);
+});
